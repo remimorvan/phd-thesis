83e0e9fa86db9f3e27c6b9880a0192fa0c6741f3
Rémi Morvan <remi@morvan.xyz>
2025-05-19 00:51:38 +0200

    figures lattice

---
+++ tikz/prelim-databases/abstract-lattice.lua
@@ -0,0 +1,22 @@
+\begin{luacode}
+
+function make_clip()
+	tex.print(string.format("\\clip (0,%f) -- (%f,0) -- (0,%f) -- (%f, 0) -- cycle;", -height, width, height, -width))
+end
+
+function draw_lattice()
+	tex.print(string.format("\\draw[draw=none, fill=cLightGrey, fill opacity=.4] (0,%f) -- (%f,0) -- (0,%f) -- (%f, 0) -- cycle;", -height, width, height, -width))
+end
+
+function draw_cq(x, y, colour, label)
+	tex.print(string.format("\\begin{scope}[xshift=%f cm, yshift=%f cm]", x, y+height))
+	tex.print(string.format("\\draw[draw=%s, fill=%s, fill opacity=.4] (0,%f) -- (%f,0) -- (0,%f) -- (%f, 0) -- cycle;", colour, colour, -height, width, height, -width))
+	tex.print(string.format("\\node[circle, fill=%s, minimum size=4pt, outer sep=1pt] (%s) at (0,%f) {};", colour, label, -height))
+	tex.print("\\end{scope}")
+end
+
+function draw_db(x, y, colour, label)
+	tex.print(string.format("\\node[circle, fill=%s, minimum size=2pt, outer sep=1pt] (%s) at (%f,%f) {};", colour, label, x, y))
+end
+
+\end{luacode}
